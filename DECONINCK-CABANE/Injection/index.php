a8cc34c64b529b1202909d9ccb49bba19ce8310b
Antonin CABANE <antonin.cabane@gmail.com>
2020-02-04 08:51:28 +0100

    Add files via upload

---
+++ DECONINCK-CABANE/Injection/index.php
@@ -0,0 +1,27 @@
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <title>DECONINCK-CABANE</title>
+  </head>
+<body>
+
+    <form action="aF2AES2.php?d=102846" method="post" class="col-lg-5">
+        <div class="form-group">
+            <label for="id">Identifiant Admin</label>
+            <input type="text" class="form-control" name="id" aria-describedby="emailHelp" placeholder="Enter id admin">
+        </div>
+        <div class="form-group">
+            <label for="mdp">Mot de passe admin</label>
+            <input type="password" class="form-control" name="mdp" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</body>
+
