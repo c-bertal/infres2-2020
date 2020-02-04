1d4ac373a189f96ec09cdd7c87d8bc9224ec81ae
louisplt16 <louispaultes@gmail.com>
2020-02-04 09:07:33 +0100

    injection et buffer

---
+++ PAULTES/Injection/Site/testmysql.php
@@ -0,0 +1,15 @@
+<?php
+$user = 'root';
+$password = '';
+$database = '';
+$port = NULL; 
+$mysqli = new mysqli('127.0.0.1', $user, $password, $database, $port);
+
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_errno . ') '
+            . $mysqli->connect_error);
+}
+echo '<p>Connection OK '. $mysqli->host_info.'</p>';
+echo '<p>Server '.$mysqli->server_info.'</p>';
+$mysqli->close();
+?>
